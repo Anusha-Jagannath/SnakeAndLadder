6b0146aa77f5c178487ec89ffd29771c713fb3a4
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-23 20:42:05 -0700

    added die rolling

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,10 @@
 public class SnakeLadder{
 	public static void main(String[] args){
+		int position=0;
+		int board[]=new int[100];
 		System.out.println("Welcome to snake and ladder game");
+		int value=(int)Math.floor(Math.random()*10)%6;
+		System.out.println("Die value is "+value);
+		
 	}
 }
