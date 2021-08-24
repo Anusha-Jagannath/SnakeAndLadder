ba3fe865ab6c0938ce802dbc376ccb4b11a7a9b1
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-24 10:52:18 -0700

    modified no move condition

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,34 +1,42 @@
-public class SnakeLadder{
-	public static final int NO_PLAY=1;
-	public static final int LADDER=2;
-	public static final int SNAKE=3;
-	public static void main(String[] args){
-		int position = 0;
-		System.out.println("Welcome to snake and ladder game");	
-		System.out.println("Single player is at postion "+position);
-		int dieNum = (int)Math.floor(Math.random()*10)%6;
-		System.out.println("Die value is "+dieNum);
-		while(position!=100){
-		if(dieNum!=0){
+import java.util.Random;
+
+public class SnakeLadder {
+	public static final int NO_PLAY = 1;
+	public static final int LADDER = 2;
+	public static final int SNAKE =3;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Problem");
 		
-		int choice = (int)Math.floor(Math.random()*10)%3;
-		System.out.println("Choice is "+choice);
-		if(choice == NO_PLAY){
-			System.out.println("No play");
+
+		int position = 0;
+		System.out.println("Single player is at postion "+position);
+		
+		Random random = new Random();
+		
+		while(position != 100) {
+			int dieNum = random.nextInt(7-1)+1;
+			System.out.println("Dice number : "+dieNum);
+		
+			int choice = random.nextInt(4-1)+1;
+		
+			if(choice == NO_PLAY){
+				System.out.println("No Play");
+			}else if(choice == LADDER) {
+				System.out.println("Player inclined by "+dieNum);
+				position+=dieNum;
+			}else if(choice == SNAKE) {
+				System.out.println("Player declined by "+dieNum);
+				position-=dieNum;
+			}
+			if(position<0)
+				position = 0;
+			
+			if(position>100)
+				position-= dieNum;
+		
 		}
-		else if(choice == LADDER){
-			System.out.println("Player got ladder");
-			System.out.println("Player moves by "+dieNum);
-			position+=dieNum;
-		}
-		else if(choice == SNAKE){
-			System.out.println("Player declined by "+dieNum);
-			position-=dieNum;
-		}
-		if(position<0)
-			position = 0;
-		if(position>100)
-	    }		position-=dieNum;
+			
 	}
-   }
+
 }
