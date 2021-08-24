9bf73efcd670079d508599bb04d292304e8bf9ca
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-24 06:00:20 -0700

    ladder snake and no play condition added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,29 @@
 public class SnakeLadder{
+	public static final int NO_PLAY=1;
+	public static final int LADDER=2;
+	public static final int SNAKE=3;
 	public static void main(String[] args){
-		System.out.println("Welcome to snake and ladder game");
+		int position = 0;
+		System.out.println("Welcome to snake and ladder game");	
+		System.out.println("Single player is at postion "+position);
+		int dieNum = (int)Math.floor(Math.random()*10)%6;
+		System.out.println("Die value is "+dieNum);
+		if(dieNum!=0){
+		
+		int choice = (int)Math.floor(Math.random()*10)%3;
+		System.out.println("Choice is "+choice);
+		if(choice == NO_PLAY){
+			System.out.println("No play");
+		}
+		else if(choice == LADDER){
+			System.out.println("Player got ladder");
+			System.out.println("Player moves by "+dieNum);
+			position+=dieNum;
+		}
+		else if(choice == SNAKE){
+			System.out.println("Player declined by "+dieNum);
+			position-=dieNum;
+		}
 	}
+   }
 }
