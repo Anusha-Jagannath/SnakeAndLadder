d4fa4f542d8f9a31331861bb2f9d569fc9ff5fb2
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-24 10:48:50 -0700

    code modified by changing while condition

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,37 +1,33 @@
-public class SnakeLadder{
+import java.util.Random;
+
+public class SnakeLadder {
 	public static final int NO_PLAY = 1;
 	public static final int LADDER = 2;
 	public static final int SNAKE =3;
 
-	public static void main(String[] args){
-		int position = 0;
-		System.out.println("Welcome to snake and ladder game");
-		System.out.println("Single player is at postion "+position);
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Problem");
+		
 
-		while(position!=100){
-		int dieNum=(int)Math.floor(Math.random()*10)%6;
-		if(dieNum!=0){
+		int position = 0;
+		System.out.println("Single player is at postion "+position);
+		
+		Random random = new Random();
+		int dieNum = random.nextInt(7-1)+1;
 		System.out.println("Dice number : "+dieNum);
-		int choice=(int)Math.floor(Math.random()*10)%3;
+		
+		int choice = random.nextInt(4-1)+1;
+		
 		if(choice == NO_PLAY){
 			System.out.println("No Play");
-		}
-		else if(choice == LADDER) {
+		}else if(choice == LADDER) {
 			System.out.println("Player inclined by "+dieNum);
 			position+=dieNum;
-		}
-		else if(choice == SNAKE) {
+		}else if(choice == SNAKE) {
 			System.out.println("Player declined by "+dieNum);
 			position-=dieNum;
 		}
-		if(position<0)
-			position = 0;
-
-	 
-             }
+			
 	}
-		
 
-
-    }
 }
