dd99dbb8acc165b84d736c5af7909488d7994e54
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-24 10:42:52 -0700

    added random function

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,9 +1,11 @@
+import java.util.Random;
 public class SnakeLadder{
 	public static void main(String[] args){
 		int position=0;
 		System.out.println("Welcome to snake and ladder game");
 		System.out.println("Single player is at postion "+position);
-		int dieNum=(int)Math.floor(Math.random()*10)%6;
+		Random random = new Random();
+		int dieNum = random.nextInt(7-1)+1;
 		System.out.println("Die value is "+dieNum);
 		
 	}
